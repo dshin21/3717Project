ea8a9dfc71c74d69db74af92b0e3e79c73fef228
Daniel Shin <danielshin234@gmail.com>
2018-10-04 13:13:54 -0700

    cleaned up code

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,13 @@
+//npm modules
 const fs = require("fs");
 const MongoClient = require('mongodb').MongoClient;
+//
+
+const url = "mongodb://localhost:27017/";
 
 
 let BUS_STOPS = fs.readFileSync('JSONs/BUS_STOPS.json');
 let rawData = JSON.parse(BUS_STOPS);
-
 let myobj = [];
 
 for (let i = 0; i < rawData.features.length; i++) {
@@ -13,15 +16,14 @@ for (let i = 0; i < rawData.features.length; i++) {
     myobj[i] = {"stopName": stopName, "lat": coordinates[0], "lng": coordinates[1]};
 }
 
-
-const url = "mongodb://localhost:27017/";
-
 MongoClient.connect(url, function (err, db) {
     if (err) throw err;
-    var dbo = db.db("test");
+    let dbo = db.db("test");
 
     dbo.collection("Bus_Stops").insertMany(myobj, function (err, res) {
         if (err) throw err;
         db.close();
     });
 });
+
+//TODO: do the rest of the API calls
